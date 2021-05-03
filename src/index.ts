e3e36ef86cc496b2653555c8bcbc188192fbaf03
200610313@my.xu.edu.ph <200610313@my.xu.edu.ph>
2021-05-03 14:26:00 +0800

    fixed typescript

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,37 @@
-const puppeteer = require("puppeteer");
+import _ from "lodash";
+import puppeteer from "puppeteer";
+const elearnUrl = "https://elearn.xu.edu.ph";
 
+const userDetails = { username: "200610313", password: "Augustus_10" };
+const selector = {
+  username: "#username",
+  password: "#password",
+};
+
+const hehe: number = 2;
 (async () => {
   const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: false,
+    // headless: true,
+    // args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
-  await page.goto("https://example.com");
-  await page.screenshot({ path: "example.png" });
-
+  await page.goto(elearnUrl);
+  await page.waitForSelector(selector["username"]);
+  await page.waitForSelector(selector["password"]);
+  await page.evaluate(() => {});
+  await page.$eval(
+    "input[name=username]",
+    (el: any) => (el.value = userDetails.username),
+    { userDetails }
+  );
+  await page.$eval(
+    "input[name=password]",
+    (el: any) => (el.value = userDetails.password),
+    { userDetails }
+  );
   console.log("SUCCESS!");
-  await browser.close();
+  // await page.screenshot({ path: "example.png" });
+  // await browser.close();
 })();
