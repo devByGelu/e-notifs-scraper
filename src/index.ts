730ae39991c5c428609d78acb7af5edde82a7f92
200610313@my.xu.edu.ph <200610313@my.xu.edu.ph>
2021-05-03 12:49:36 +0800

    added puppeteer launch options

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
 
   const page = await browser.newPage();
   await page.goto("https://example.com");
   await page.screenshot({ path: "example.png" });
 
+  console.log("SUCCESS");
   await browser.close();
 })();
