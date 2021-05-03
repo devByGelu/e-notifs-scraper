7d3215c0602b4fa65643469fb559a8b834971ea7
200610313@my.xu.edu.ph <200610313@my.xu.edu.ph>
2021-05-03 12:45:52 +0800

    added puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  await page.screenshot({ path: "example.png" });
+
+  await browser.close();
+})();
